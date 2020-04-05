2539ceca0d31f09775e196241d6f1ce54421010c
Christopher Dougherty <christophertdoc@gmail>
2020-04-05 19:14:37 -0400

    ..

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+const Data = () => {
+    useEffect(() => {
+        console.log('useEffect')
+    })
+    return (
+        <div>
+            <h2>Data</h2>
+        </div>
+    )
+}
+
+export default Data
