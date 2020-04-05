6042760f26d2fd2b33ee275271462f496d794576
Christopher Dougherty <christophertdoc@gmail>
2020-04-05 19:46:59 -0400

    request

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+
 const Data = () => {
+
+    const [response, setResponse] = useState('')
+
     useEffect(() => {
-        console.log('useEffect')
-        axios.get(`http://api.open-notify.org/iss-now.json`)
-        .then(response => {
-            console.log('response', response)
-        })
-    })
+        const interval = setInterval(() => {
+            axios.get(`http://api.open-notify.org/iss-now.json`)
+            .then(response => {
+                console.log('response', response.data)
+                setResponse(response.data)
+            })
+        }, 5000);
+        return () => clearInterval(interval);
+      }, []);
+
+
     return (
         <div>
-            <h2>Data</h2>
+            <h2>International Space Station Position</h2>
+            {response &&
+                <div>
+                    <h3>Latitude: { response.iss_position.latitude }</h3>
+                    <h3>Longitude: { response.iss_position.longitude }</h3>
+                </div>
+            }
         </div>
     )
 }
