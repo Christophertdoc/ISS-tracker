a673c5130559d1fecfc1769013ab7bb82370b11a
Christopher Dougherty <christophertdoc@gmail>
2020-04-05 19:21:01 -0400

    request

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Data = () => {
     useEffect(() => {
         console.log('useEffect')
+        axios.get(`http://api.open-notify.org/iss-now.json`)
+        .then(response => {
+            console.log('response', response)
+        })
     })
     return (
         <div>
